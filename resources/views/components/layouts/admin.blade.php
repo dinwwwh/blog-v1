0a90fed6470998f1b2eb2ab3f741bdc4ebba508c
Le Dinh <dinhdjj@gmail.com>
2021-09-09 14:29:08 +0700

    INIT `laravel` `laravel-mix` `tailwindcss` `barryvdh/laravel-ide-helper`

---
+++ resources/views/components/layouts/admin.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>test</title>
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.base>
+    <h1 class="text-gray-800">xin chao</h1>
+</x-layouts.base>
