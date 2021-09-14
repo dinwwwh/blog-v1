7210bfc4eee83ec0d00b3c515b2e4f6d5914a7a5
Le Dinh <dinhdjj@gmail.com>
2021-09-14 16:58:02 +0700

    chore: remove redundant class

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
