1a2f0c71e8553b7786b56be7f36b35b17c23f751
Le Dinh <dinhdjj@gmail.com>
2021-09-17 09:20:16 +0700

    fix: include local backup files when backup
    This issue occur when backup to cloud like `dropbox` but not backup in local disk

--- config/backup.php
+++ config/backup.php
@@ -29,6 +29,7 @@ return [
                 'exclude' => [
                     base_path('vendor'),
                     base_path('node_modules'),
+                    base_path('storage/app/Laravel'), #Ignore backup files of 'local' disk
                 ],
 
                 /*
